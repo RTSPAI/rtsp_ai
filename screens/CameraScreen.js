f55b58fd93970b6e2e0e4663dd53786e4f9b6150
Max Bagatini Alves <maxbagatini@hotmail.com>
2025-02-17 17:24:29 -0500

    add template frame processor
    currently only prints dimensions and timestamp to ensure it stops running if app is no longer in focus

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { FIREBASE_AUTH } from '../firebaseConfig';
 import { useAuthContext, resetScreens } from '../context/AuthContext';
-import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera';
+import { Camera, useCameraDevice, useCameraPermission, useFrameProcessor } from 'react-native-vision-camera';
 import { useIsFocused } from '@react-navigation/native';
 import { useAppState } from '@react-native-community/hooks';
 
@@ -34,6 +34,11 @@ const CameraScreen = ({ route, navigation }) => {
         requestPermission();
     }
 
+    const frameProcessor = useFrameProcessor((frame) => {
+        'worklet'
+        console.log(`${Date.now()} | Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`)
+    }, [])
+
     return (
         <View style={styles.container}>
             {hasPermission ? (
@@ -41,6 +46,8 @@ const CameraScreen = ({ route, navigation }) => {
                     style={StyleSheet.absoluteFill}
                     device={device}
                     isActive={isActive}
+                    frameProcessor={frameProcessor}
+                    enableFpsGraph
                 />
             ) : (
                 <Text style={styles.text}>No camera permissions given.</Text>
