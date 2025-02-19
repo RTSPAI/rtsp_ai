f065bab23faa6e5437d258b97fa867b20c0889f5
mpryce954 <mpryce119@gmail.com>
2025-02-18 20:16:30 -0500

    Updated HomeScreen UI/Added new icons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { FIREBASE_AUTH } from '../firebaseConfig';
 import { useAuthContext, resetScreens } from '../context/AuthContext';
-import Ionicons from '@expo/vector-icons/Ionicons';
 import ExerciseList from '../components/ExerciseList';
+
+
 
 const HomeScreen = ({ navigation }) => {
     const { user, loadingUser } = useAuthContext();
@@ -20,32 +22,56 @@ const HomeScreen = ({ navigation }) => {
         return null;
     }
 
-    return (
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View style={styles.container}>
-                <Ionicons.Button name="person" size={32} onPress={() => navigation.navigate("Profile")}>Profile</Ionicons.Button>
-                <View style={styles.space}></View>
-                <Text style={styles.text}>Insert some welcome / simple tutorial text here...</Text>
-                <Text style={styles.text}>Insert list of available exercises here...</Text>
-                <ExerciseList navigation={navigation}/>
-            </View>
-        </TouchableWithoutFeedback>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>AI Trainer</Text>
+      <Text style={styles.subtitle}>Select an exercise or auto detect to begin workout!</Text>
+
+      <TouchableOpacity style={styles.profileButton} onPress={() => navigation.navigate('Profile')}>
+      <Text style={styles.profileButtonText}>Profile</Text>
+      </TouchableOpacity>
+
+      {/* Component for displaying exercise list */}
+      <ExerciseList navigation={navigation} />
+      
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text: {
-        fontSize: 16,
-    },
-    space: {
-        width: 20,
-        height: 20,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#E4E4E4',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 5,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 20,
+    textAlign: 'center',
+    paddingHorizontal: 20,
+  },
+  profileButton: {
+    backgroundColor: '#6D8E93',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  profileButtonText: {
+    color: '#FFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
 
 export default HomeScreen;
+
